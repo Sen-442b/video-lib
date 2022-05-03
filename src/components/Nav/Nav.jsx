589ecfc728fe7b1fe809cb9ebb21a7a9b3da33fa
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2022-05-03 15:27:12 +0530

    feat:home route added

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav className="nav-container">
+      <div>
+        <i class="fa-solid fa-bars"></i>
+      </div>
+      <div>Logo</div>
+      <div>Login</div>
+    </nav>
+  );
+};
+
+export default Nav;
