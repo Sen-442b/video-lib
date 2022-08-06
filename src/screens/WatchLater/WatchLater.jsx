0893d5e682a065c972e9bf61b97f25c6606e3102
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2022-08-06 23:10:48 +0530

    feat : sort functionality added

--- src/screens/WatchLater/WatchLater.jsx
+++ src/screens/WatchLater/WatchLater.jsx
@@ -9,7 +9,27 @@ import WatchLaterCard from "./WatchLaterCard";
 const WatchLater = () => {
   const dispatch = useDispatch();
   const watchLaterState = useSelector((storeState) => storeState.watchLater);
+  const [isSortMenuOpen, setIsSortMenuOpen] = useState(false);
+  const [dateSortType, setDateSortType] = useState("");
   const { watchLater } = watchLaterState;
+  ["2", "3"];
+  const sortByDate = (dataArr, type) => {
+    if (type === "newToOld") {
+      return [...dataArr].sort((videoCardOne, videoCardTwo) => {
+        return (
+          new Date(videoCardTwo.uploadDate) - new Date(videoCardOne.uploadDate)
+        );
+      });
+    } else if (type === "oldToNew") {
+      return [...dataArr].sort((videoCardOne, videoCardTwo) => {
+        return (
+          new Date(videoCardOne.uploadDate) - new Date(videoCardTwo.uploadDate)
+        );
+      });
+    }
+    return dataArr;
+  };
+
   return (
     <div className="watch-later-wrapper">
       <div className="watch-later-overview">
@@ -24,7 +44,7 @@ const WatchLater = () => {
         <div className="overview-content">
           <h3 className="typography-heading-3">Watch Later</h3>
           <div className="overview-content-actions">
-            <p>230 videos</p>
+            <p>{watchLater.length} videos</p>
             <i className="fas fa-random" title="Shuffle"></i>
             <i className="fas fa-plus" title="Add Videos"></i>
           </div>
@@ -33,17 +53,42 @@ const WatchLater = () => {
 
       <div className="watch-later-content">
         <div className="content-sort">
-          <i
-            className="fas fa-sort-amount-up"
-            id="watch-later-sort"
+          <span
+            style={{ backgroundColor: "red" }}
             role="button"
-          ></i>
-          <label htmlFor="watch-later-sort" className="fw-heavy">
-            SORT
-          </label>
+            onClick={() => setIsSortMenuOpen((prevBool) => !prevBool)}
+            onBlur={() => {
+              console.log("blur event triggered");
+              setIsSortMenuOpen(false);
+            }}
+            tabIndex="0"
+          >
+            <i className="fas fa-sort-amount-up" id="watch-later-sort"></i>
+            <label htmlFor="watch-later-sort" className="fw-heavy">
+              SORT
+            </label>
+            {isSortMenuOpen && (
+              <div className="menu-wrapper-sort">
+                <div
+                  className="menu-item"
+                  role="button"
+                  onClick={() => setDateSortType("newToOld")}
+                >
+                  <p>Date added (newest)</p>
+                </div>
+                <div
+                  className="menu-item"
+                  role="button"
+                  onClick={() => setDateSortType("oldToNew")}
+                >
+                  <p>Date added (oldest)</p>
+                </div>
+              </div>
+            )}
+          </span>
         </div>
         {watchLater.length !== 0 &&
-          watchLater.map((watchLaterObj) => {
+          sortByDate(watchLater, dateSortType).map((watchLaterObj) => {
             return (
               <WatchLaterCard
                 key={watchLaterObj._id}
